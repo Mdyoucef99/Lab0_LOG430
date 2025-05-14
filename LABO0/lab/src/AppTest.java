590cc3fefaa2fd7034d8e0a33a842bee74f3ee85
Youcef <mekki.d.youcef@outlook.fr>
2025-05-14 09:37:14 -0400

    JUnit tests

---
+++ LABO0/lab/src/AppTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+@Test
+public void test1()
+{
+    int a=0;
+    int b=1;
+
+    int c = a+b;
+    assertEquals(1, c);
+
+}
+
+@Test
+public void test2()
+{
+    int a=1;
+    int b=1;
+
+    int c = a+b;
+    assertEquals(2, c);
+
+}
+
+
+
+    
+}
